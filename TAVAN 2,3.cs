5514d87719429d603ea035eb7ed087339613ac50
javad2568 <117682959+javad2568@users.noreply.github.com>
2022-11-16 12:44:27 -0800

    Update TAVAN 2,3.cs

--- TAVAN 2,3.cs
+++ TAVAN 2,3.cs
@@ -10,20 +10,16 @@ namespace ConsoleApplication9
     {
         static void Main(string[] args)
         {
-
-            int n;
-
-            Console.WriteLine("Enter your number: ");
-            n = Convert.ToInt32(Console.ReadLine());
-
-            Power(n);
+            int X;
+          Console.WriteLine("GIVE ME ADAD ")
+              X = Convert.ToInt32(Console.ReadLine());
+           Power(X);
         }
-
-        static void Power(int n)
+    static void Power(int X)
         {
-            Console.WriteLine(Math.Pow(n, 2));
-            Console.WriteLine(Math.Pow(n, 3));
-            Console.ReadKey();
+            Console.WriteLine(Math.Pow(X, 2));
+         Console.WriteLine(Math.Pow(X, 3));
+               Console.ReadKey();
         }
     }
 }
